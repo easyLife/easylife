96d572ac11219f3fee52a8ad55a2a73002034aa5
Renich Bon Ciric <renich@woralelandia.com>
2011-01-14 01:10:49 -0600

    removed .noarch from package name

--- lib/nicemenus.sh
+++ lib/nicemenus.sh
@@ -3,7 +3,7 @@ NiceMenus() {
     echo "[$FUNCNAME]"
     
     yum install -y games-menus preferences-menus \
-                   security-menus multimedia-menus.noarch
+                   security-menus multimedia-menus
     
     [[ $? != 0 ]] && ErrMsg "Could not install package" && return 1
 }
